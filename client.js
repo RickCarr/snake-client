33385d5909ef5acf608ff76b50abd118452daf4c
RickCarr <rick.carr@telus.com>
2022-11-23 19:00:14 -0800

    next step

--- client.js
+++ client.js
@@ -1,24 +1,27 @@
 const net = require("net");
+const { IP, PORT } = require('./constants');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541',
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  
+
   conn.on('connect', () => {
     conn.write('Name: rrc');
-  
-    console.log('Established Connection');
+    console.log('Welcome to Move or Die!');
+    console.log('Movements: [h] = left, [j] = down, [k] = up, [l] = right');
+    console.log('Snake Talk: [1] = Hello, [2] = World, [3] = I am a, [4] = talking, [5] = snake!');
+    console.log('Exit Command: [Ctrl]+[c]');
   });
   conn.on('data', (incomingData) => {
     console.log('Incoming Data :', incomingData);
   });
   return conn;
 };
-const connection = connect();
 
+const connection = connect();
 module.exports = { connection };
