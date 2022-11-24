cd20cfa51bc064219d94ba64759c24335e9cc5e6
RickCarr <rick.carr@telus.com>
2022-11-23 17:52:50 -0800

    next step

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
 let connection;
 
 const handleUserInput = function(key) {
+  //movement
   if (key === 'h') {
     connection.write('Move: left');
   }
@@ -13,9 +14,26 @@ const handleUserInput = function(key) {
   if (key === 'l') {
     connection.write('Move: right');
   }
+  //exit command
   if (key === '\u0003') {
     process.stdout.write('Emergency Exit!\n');
     process.exit();
+  }
+  //communication
+  if (key === '1') {
+    connection.write('Say: Hello');
+  }
+  if (key === '2') {
+    connection.write('Say: World');
+  }
+  if (key === '3') {
+    connection.write('Say: I am a');
+  }
+  if (key === '4') {
+    connection.write('Say: talking');
+  }
+  if (key === '5') {
+    connection.write('Say: snake!');
   }
 };
 const setupInput = (conn) => {
